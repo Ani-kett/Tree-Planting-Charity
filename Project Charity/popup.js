dd94771467fd3ff153010a95a592d983626db804
Aniket <103031504+Ani-kett@users.noreply.github.com>
2023-09-29 21:33:12 +0530

    Add files via upload

---
+++ Project Charity/popup.js
@@ -0,0 +1,13 @@
+
+function donate(){
+    alert("DONATE TO PLANT TRESS TODAY");
+}
+
+function plant(){
+    alert("THANKS FOR THE INITIATIVE");
+}
+
+function submit(){
+    alert("SUBMITTED");
+}
+
